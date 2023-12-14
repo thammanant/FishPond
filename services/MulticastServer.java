f16e9e67c0e9c7f1e088f42027a6cf855869c5f2
Pun <80048553+thammanant@users.noreply.github.com>
2023-12-14 20:54:52 +0700

    modify as APi

--- services/MulticastServer.java
+++ services/MulticastServer.java
@@ -6,17 +6,23 @@ import java.net.InetAddress;
 import java.net.MulticastSocket;
 
 public class MulticastServer {
-    private static final int SERVER_PORT = 4446;
     private static final String MULTICAST_ADDRESS = "230.0.0.0";
 
+    // for testing
     public static void main(String[] args) {
-        runServer();
+        int portNumber = 12345;
+        String clock = "12";
+
+        String receivedMessage = runServer(portNumber, clock);
+        System.out.println(STR."Received message from server: \{receivedMessage}");
     }
 
-    private static void runServer() {
+    private static String runServer(int portNumber, String clock) {
+        StringBuilder receivedMessage = new StringBuilder();
+
         try {
             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-            MulticastSocket socket = new MulticastSocket(SERVER_PORT);
+            MulticastSocket socket = new MulticastSocket(portNumber);
             socket.joinGroup(group);
 
             System.out.println("Server: Waiting for connection...");
@@ -27,14 +33,12 @@ public class MulticastServer {
                 socket.receive(packet);
 
                 String received = new String(packet.getData(), 0, packet.getLength());
-                System.out.println("Server received: " + received);
-
-                if (received.equals("connected to the server")) {
-                    System.out.println("Server: 1 user connected.");
-                }
+                System.out.println(STR."Server received: \{received}");
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        return receivedMessage.toString();
     }
 }
