a53acec136450d4827359ed4d4b7bb105996abef
jimmy666k <64011397@kmitl.ac.th>
2023-12-15 23:11:24 +0700

    update drawfish method

--- services/fish.java
+++ services/fish.java
@@ -1,29 +1,30 @@
 package services;
 import java.util.Scanner;
-
+import services.startup;
+import java.util.Random;
 public class fish {
-    public void drawFish(){
-        System.out.print("Please select fish type: \n");
-
+    public void bubbleFish(){
         
-        //buble fish
-        System.out.println("Type 1)\n");
         System.out.println("               O  o");
         System.out.println("          _\\_   o");
         System.out.println("       \\/  o\\ .");
         System.out.println("       //\\___=");
         System.out.println("          ''");
         System.out.println("\n");
-        //shark
-        System.out.println("Type 2)\n");
+    }
+
+    public void shark(){
+        
         System.out.println("      .            ");
         System.out.println("\\_____\\)\\_____");
         System.out.println("/--v____ __`<       ");
         System.out.println("        )/           ");
         System.out.println("        '            ");
         System.out.println("\n");
-        //triangl fish
-        System.out.println("Type 3)\n");
+    }
+
+    public void triangleFish(){
+        
         System.out.println("     |\\    o");
         System.out.println("    |  \\    o");
         System.out.println("|\\ /    .\\ o");
@@ -32,8 +33,9 @@ public class fish {
         System.out.println("    |  /");
         System.out.println("     |/");
         System.out.println("\n");
-        //sea horse
-        System.out.println("Type 4)\n");
+    }
+
+    public void seahorse(){
         System.out.println("     \\/)/)");
         System.out.println("    _'  oo(_.-.");
         System.out.println("  /'.     .---'");
@@ -44,8 +46,10 @@ public class fish {
         System.out.println("    (  (,.");
         System.out.println("     '-.-'");
         System.out.println("\n");
-        //puffle fish
-        System.out.println("Type 5)\n");
+    }
+
+    public void pufflefish(){
+        
         System.out.println("     .");
         System.out.println("                          A       ;");
         System.out.println("                |   ,--,-/ \\---,-/|  ,");
@@ -68,6 +72,64 @@ public class fish {
         System.out.println("          `|/-.      \\ /   \\ /,---`\\            ");
         System.out.println("                /   `._____V_____V'");
         System.out.println("                           '     '");
+    }
+
+    public void drawFish(){
+        System.out.print("Please select fish type: \n");
+        System.out.println("Type 1)\n");
+        bubbleFish();
+        System.out.println("Type 2)\n");
+        shark();
+        System.out.println("Type 3)\n");
+        triangleFish();
+        System.out.println("Type 4)\n");
+        seahorse();
+        System.out.println("Type 5)\n");
+        pufflefish();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter fish type: ");
+        String userChoice = input.nextLine();
+
+        Random random = new Random();
+        int randomNum = random.nextInt(10);
+
+        switch(userChoice){
+            case "1":
+                System.out.println("You have selected bubble fish");
+                bubbleFish();
+                break;
+            case "2":
+                System.out.println("You have selected shark");
+                shark();
+                break;
+            case "3":
+                System.out.println("You have selected triangle fish");
+                triangleFish();
+                break;
+            case "4":
+                System.out.println("You have selected seahorse");
+                seahorse();
+                break;
+            case "5":
+                System.out.println("You have selected pufflefish");
+                pufflefish();
+                break;
+            case "exit":
+                System.out.println("Quitting...");
+                break;
+            default:
+                System.out.println("Invalid input, please type number between 1-5");
+                break;
+        }
+        input.close();
+        
+        startup mainmenu = new startup(0);
+        mainmenu.start();
+
+        
+        
+        
+        
     }
 }
 
