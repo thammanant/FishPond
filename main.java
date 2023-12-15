467bd3b692adcdb8690376433bde0aa03f2fffd2
jimmy666k <64011397@kmitl.ac.th>
2023-12-15 21:30:37 +0700

    add fish class

--- main.java
+++ main.java
@@ -1,9 +1,13 @@
 import services.startup;
 import services.shutdown;
+import services.fish;
 public class main {
     public static void main(String[] args) {
-        shutdown terminate = new shutdown();
-        terminate.loop();
+        // shutdown terminate = new shutdown();
+        // terminate.loop();
+        fish drawfish = new fish();
+        drawfish.drawFish();
+        
        
     }
 }
---
+++ services/fish.java
@@ -0,0 +1,73 @@
+package services;
+import java.util.Scanner;
+
+public class fish {
+    public void drawFish(){
+        System.out.print("Please select fish type: \n");
+
+        
+        //buble fish
+        System.out.println("Type 1)\n");
+        System.out.println("               O  o");
+        System.out.println("          _\\_   o");
+        System.out.println("       \\/  o\\ .");
+        System.out.println("       //\\___=");
+        System.out.println("          ''");
+        System.out.println("\n");
+        //shark
+        System.out.println("Type 2)\n");
+        System.out.println("      .            ");
+        System.out.println("\\_____\\)\\_____");
+        System.out.println("/--v____ __`<       ");
+        System.out.println("        )/           ");
+        System.out.println("        '            ");
+        System.out.println("\n");
+        //triangl fish
+        System.out.println("Type 3)\n");
+        System.out.println("     |\\    o");
+        System.out.println("    |  \\    o");
+        System.out.println("|\\ /    .\\ o");
+        System.out.println("| |       (");
+        System.out.println("|/ \\     /");
+        System.out.println("    |  /");
+        System.out.println("     |/");
+        System.out.println("\n");
+        //sea horse
+        System.out.println("Type 4)\n");
+        System.out.println("     \\/)/)");
+        System.out.println("    _'  oo(_.-.");
+        System.out.println("  /'.     .---'");
+        System.out.println("/'-./    (");
+        System.out.println(")     ; __\\");
+        System.out.println("\\_.'\\ : __|");
+        System.out.println("     )  _/");
+        System.out.println("    (  (,.");
+        System.out.println("     '-.-'");
+        System.out.println("\n");
+        //puffle fish
+        System.out.println("Type 5)\n");
+        System.out.println("     .");
+        System.out.println("                          A       ;");
+        System.out.println("                |   ,--,-/ \\---,-/|  ,");
+        System.out.println("               _|\\,'. /|      /|   `/|-. ");
+        System.out.println("           \\`.'    /|      ,            `;.");
+        System.out.println("          ,\\   A     A         A   A _ /| `.;");
+        System.out.println("        ,/  _              A       _  / _   /|  ;");
+        System.out.println("       /\\  / \\   ,  ,           A  /    /     `/|");
+        System.out.println("      /_| | _ \\         ,     ,             ,/  \\");
+        System.out.println("     // | |/ `.\\  ,-      ,       ,   ,/ ,/      \\ /");
+        System.out.println("     / @| |@  / /'   \\  \\      ,              >  /|    ,--.");
+        System.out.println("    |\\_/   \\_/ /      |  |           ,  ,/        \\  ./' __:..");
+        System.out.println("    |  __ __  |       |  | .--.  ,         >  >   |-'   /     `");
+        System.out.println("  ,/| /  '  \\ |       |  |     \\      ,           |    /");
+        System.out.println(" /  |<--.__,->|       |  | .    `.        >  >    /   (");
+        System.out.println("/_| \\  ^  /  \\     /  /   `.    >--            /^\\   |");
+        System.out.println("      \\___/    \\   /  /      \\__'     \\   \\   \\/   \\  |");
+        System.out.println("       `.   |/          ,  ,                  /`\\    \\  )");
+        System.out.println("         \\  '  |/    ,       V    \\          /        `-\\");
+        System.out.println("          `|/-.      \\ /   \\ /,---`\\            ");
+        System.out.println("                /   `._____V_____V'");
+        System.out.println("                           '     '");
+    }
+}
+
