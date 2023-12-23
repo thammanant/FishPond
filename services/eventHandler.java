8a6287fbca7f9429cc9209200107ec93d7954579
Pun <80048553+thammanant@users.noreply.github.com>
2023-12-23 22:06:26 +0700

    add writeToLog function

--- services/eventHandler.java
+++ services/eventHandler.java
@@ -1,4 +1,29 @@
 package services;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class eventHandler {
+    public static boolean writeToLog(String command, Integer fishID, Integer pondID, Integer clock) {
+        return writeToLog(command, fishID, pondID, clock, ""); // Call the main function with default msg
+    }
+
+    public static boolean writeToLog(String command, Integer fishID, Integer pondID, Integer clock, String msg) {
+        try {
+            FileWriter fileWriter = new FileWriter("log.txt", true); // true for appending to the file
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Construct the log message
+            String logMessage = String.format("%s, %d, %d, %s, %d\n", command, fishID, pondID, msg, clock);
+
+            bufferedWriter.write(logMessage);
+            bufferedWriter.close();
+
+            return true; // Successfully wrote to the log file
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false; // Failed to write to the log file
+        }
+    }
 }
