4e40dae5e8c84a0d3cce9dcfc2797c102972607a
jimmy666k <64011397@kmitl.ac.th>
2024-02-01 20:52:00 +0700

    change methods name in DisplayMenu.java

--- services/DisplayMenu.java
+++ services/DisplayMenu.java
@@ -3,16 +3,17 @@ package services;
 import fishes.*;
 
 public class DisplayMenu {
-    public void displayMainMenu() {
-        System.out.println("1. Add Fish");
-        System.out.println("2. Remove Fish");
-        System.out.println("3. Move Fish");
-        System.out.println("4. Undo");
-        System.out.println("5. Redo");
-        System.out.println("6. Exit");
+    public void display_main_menu() {
+        System.out.println("1. Add fish");
+        System.out.println("2. Remove fish");
+        System.out.println("3. Draw fish pond");
+        System.out.println("4. Move fish");
+        System.out.println("5. Shutdown");
+        System.out.println("6. System report");
+        System.out.println("7. Pond report");
     }
 
-    public void addFishMenu() {
+    public void add_fish_menu() {
         System.out.print("Please select fish type: \n");
         System.out.println("Type 1)\n");
         BubbleFish bubbleFish = new BubbleFish(0, 0, 0);
