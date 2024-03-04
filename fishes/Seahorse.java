fd7564d8dd04d980a3fb2bc8c3fb1e1e8edb2f85
chananon-n <91945879+chananon-n@users.noreply.github.com>
2024-03-04 22:57:45 +0700

    update fish ID

--- fishes/Seahorse.java
+++ fishes/Seahorse.java
@@ -9,7 +9,7 @@ public class Seahorse extends Fish{
         public Seahorse(int x, int y, int id) {
             this.x = x;
             this.y = y;
-            this.type = 3;
+            this.type = 4;
             this.id = id;
             this.imageLeft = "";
             this.imageRight = "";
--- fishes/TriangleFish.java
+++ fishes/TriangleFish.java
@@ -9,7 +9,7 @@ public class TriangleFish extends Fish{
         public TriangleFish(int x, int y, int id) {
             this.x = x;
             this.y = y;
-            this.type = 4;
+            this.type = 3;
             this.id = id;
             this.imageLeft = "";
             this.imageRight = "";
