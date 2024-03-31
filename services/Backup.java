a0362fbb0bb6d7f33b55f088dddcfa76c8549d01
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2024-03-31 16:45:35 +0700

    add backup function

--- services/Backup.java
+++ services/Backup.java
@@ -1,11 +1,50 @@
 package services;
 
+import java.io.*;
+
 public class Backup {
-    /*
-       1. have a backup file, that stores database information (in here)
-       2. have backup function(in here) that writes to the backup file and deletes the log file(log class)
-       3. everytime the server starts, check if the backup file exists, if it does, read from it(startup class)
-       4. monitor the main program, if it crashes, restart it and read from the backup file (in here, run parallel to the main program)
-       5.
-     */
+    public static final String backupFile = "backup.txt";
+
+    public static void backup() {
+        try {
+            FileWriter fileWriter = new FileWriter(backupFile);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            // Construct the log message
+            String backupMessage = String.format("%s, %d", Database.read_fish_fromDB(), Clock.get_current_clock());
+
+            bufferedWriter.write(backupMessage);
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void recover() {
+        try {
+            // write the backup file to the fish database(fish.json)
+            FileWriter fileWriter = new FileWriter("fish.json");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            // read the backup file and get only the fish database
+            FileReader fileReader = new FileReader(backupFile);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            StringBuilder backup = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                backup.append(line).append("\n");
+            }
+            String[] backupParts = backup.toString().split(",");
+            // write from the first continuously until the second last element
+            for (int i = 0; i < backupParts.length-1; i++) {
+                bufferedWriter.write(backupParts[i]);
+            }
+            bufferedReader.close();
+
+            // redo the tasks in the log file
+            ManageLogFile.redo_task(Integer. parseInt(backupParts[backupParts.length-1]));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
+
